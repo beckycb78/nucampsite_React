c97a1c8c61c7d4d7197bab8a9c2af54411dd4003
Rebecca Basquez <beckycb78@gmail.com>
2022-05-11 21:56:27 -0500

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
-//import { CAMPSITES } from './app/shared/CAMPSITES';
-//import React from 'react';
-//import logo from './logo.svg';
-//import { Counter } from './features/counter/Counter';
-//import { Container, Navbar, NavbarBrand } from 'reactstrap';
-//import NucampLogo from './app/assets/img/logo.png';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route 
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+        />
+      </Routes>
       <Footer />
     </div>
   );
